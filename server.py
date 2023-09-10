fe701e86210db3716fc2bc4b701eb03aad4fcc50
Liam-Reid-2000 <lreid295@gmail.com>
2023-09-10 18:33:54 +0100

    First working version

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import json
 import os
 
 # import requests
+UPLOAD_DIRECTORY = "uploads"
 
 class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -20,9 +21,31 @@ class RequestHandler(BaseHTTPRequestHandler):
       return
     
     def do_POST(self):
+      content_length = int(self.headers['Content-Length'])
+      data = self.rfile.read(content_length)
+
+      # Ensure the uploads directory exists
+      if not os.path.exists(UPLOAD_DIRECTORY):
+          os.makedirs(UPLOAD_DIRECTORY)
+
+      filepath = os.path.join(UPLOAD_DIRECTORY, "upload.obj")
+
+      with open(filepath, 'wb') as f:
+          f.write(data)
+          
+      # command = "cd .. && cd Slic3r && slic3r-console --load config.ini C:\\Users\\Microwave\\remote_slic3r\\uploads\\upload.obj"
+      command = "cd Slic3r && ./Slic3r --load /app/config.ini /app/uploads/upload.obj"
+
+      result = os.system(command)
+      
+      with open("/app/uploads/upload.gcode", 'rb') as f:
+          output_data = f.read()
+
       self.send_response(200)
-      self.send_header('Content-type', 'text/plain')
+      self.send_header('Content-Length', len(output_data))
       self.end_headers()
+      self.wfile.write(output_data)
+
 
             
 
